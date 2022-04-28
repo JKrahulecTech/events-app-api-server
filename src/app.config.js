d4e43083ab4c52fac8ecb0f07ee30f1ecc17c52b
JKrahulecTech <jkrahulec.tech@gmail.com>
2022-04-28 17:01:14 +0000

    Changed to Herman

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Top Jimmy';
+    team = 'Herman Munster';
     version = '1.0.0';
 }
 module.exports = AppConfig;
