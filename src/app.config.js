ae1eba241d613d4d10202e818b8510090251d02f
JKrahulecTech <jkrahulec.tech@gmail.com>
2022-04-29 22:05:23 +0000

    Another change to see if Jenkins does its thing

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Herman Munster';
+    team = 'Latest Dude';
     version = '1.0.0';
 }
 module.exports = AppConfig;
